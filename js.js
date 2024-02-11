032e58049b2e4f6458a3d4c6ebd5222954273029
MugnaiLorenzo <lorenzo.mugnai5@stud.unifi.it>
2024-02-11 10:35:09 +0100

    Initial commit

---
+++ js.js
@@ -0,0 +1,87 @@
+let squad1 = 0;
+let squad2 = 0;
+
+
+function s1_1(){
+    document.getElementById("squad1_1").style.display = "none";
+    squad1 = squad1 + 1;
+    if(squad1 === 3){
+        document.getElementById("squad1").style.display = "block";
+        document.getElementById("squad1_1").style.display = "none";
+        document.getElementById("squad1_2").style.display = "none";
+        document.getElementById("squad1_3").style.display = "none";
+        document.getElementById("squad2_1").style.display = "none";
+        document.getElementById("squad2_2").style.display = "none";
+        document.getElementById("squad2_3").style.display = "none";
+    }
+}
+
+function s1_2(){
+    document.getElementById("squad1_2").style.display = "none";
+    squad1 = squad1 + 1;
+    if(squad1 === 3){
+        document.getElementById("squad1").style.display = "block";
+        document.getElementById("squad1_1").style.display = "none";
+        document.getElementById("squad1_2").style.display = "none";
+        document.getElementById("squad1_3").style.display = "none";
+        document.getElementById("squad2_1").style.display = "none";
+        document.getElementById("squad2_2").style.display = "none";
+        document.getElementById("squad2_3").style.display = "none";
+    }
+}
+
+function s1_3(){
+    document.getElementById("squad1_3").style.display = "none";
+    squad1 = squad1 + 1;
+    if(squad1 === 3){
+        document.getElementById("squad1").style.display = "block";
+        document.getElementById("squad1_1").style.display = "none";
+        document.getElementById("squad1_2").style.display = "none";
+        document.getElementById("squad1_3").style.display = "none";
+        document.getElementById("squad2_1").style.display = "none";
+        document.getElementById("squad2_2").style.display = "none";
+        document.getElementById("squad2_3").style.display = "none";
+    }
+}
+
+function s2_1(){
+    document.getElementById("squad2_1").style.display = "none";
+    squad2 = squad2 + 1;
+    if(squad2 === 3){
+        document.getElementById("squad2").style.display = "block";
+        document.getElementById("squad1_1").style.display = "none";
+        document.getElementById("squad1_2").style.display = "none";
+        document.getElementById("squad1_3").style.display = "none";
+        document.getElementById("squad2_1").style.display = "none";
+        document.getElementById("squad2_2").style.display = "none";
+        document.getElementById("squad2_3").style.display = "none";
+    }
+}
+
+function s2_2(){
+    document.getElementById("squad2_2").style.display = "none";
+    squad2 = squad2 + 1;
+    if(squad2 === 3){
+        document.getElementById("squad2").style.display = "block";
+        document.getElementById("squad1_1").style.display = "none";
+        document.getElementById("squad1_2").style.display = "none";
+        document.getElementById("squad1_3").style.display = "none";
+        document.getElementById("squad2_1").style.display = "none";
+        document.getElementById("squad2_2").style.display = "none";
+        document.getElementById("squad2_3").style.display = "none";
+    }
+}
+
+function s2_3(){
+    document.getElementById("squad2_3").style.display = "none";
+    squad2 = squad2 + 1;
+    if(squad2 === 3){
+        document.getElementById("squad2").style.display = "block";
+        document.getElementById("squad1_1").style.display = "none";
+        document.getElementById("squad1_2").style.display = "none";
+        document.getElementById("squad1_3").style.display = "none";
+        document.getElementById("squad2_1").style.display = "none";
+        document.getElementById("squad2_2").style.display = "none";
+        document.getElementById("squad2_3").style.display = "none";
+    }
+}
